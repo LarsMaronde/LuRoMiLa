aadd19e91498c32695f6288c08f881448744461b
mitschl <Franke-michelle@gmx.de>
2019-06-06 19:02:41 +0200

    Klasse Produkt hinzugefügt & Frontend angefangen

--- verteilte systeme/warenverwaltung/src/main/java/com/onlineshop/warenverwaltung/demo/WarenverwaltungController.java
+++ verteilte systeme/warenverwaltung/src/main/java/com/onlineshop/warenverwaltung/demo/WarenverwaltungController.java
@@ -4,6 +4,7 @@ import com.onlineshop.warenverwaltung.demo.Produkt.Model.Produkt;
 import com.onlineshop.warenverwaltung.demo.Produkt.Repository.ProduktRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +20,19 @@ public class WarenverwaltungController {
         return "warenkorb";
     }
 
+    @ResponseBody
+    @RequestMapping("/create")
+    public String create(String name, int artikelnummer, float preis){
+        String id ="";
+        try{
+            Produkt produkt = new Produkt(name, artikelnummer, preis);
+            produktRepo.save(produkt);
+            id =  String.valueOf(produkt.getArtikelnummer());
+        }catch(Exception e){
+            return "Error creating the Produkt: " + e.toString();
+        }
+        return "Produkt succesfully created with id = " + id;
+
+    }
 
 }
