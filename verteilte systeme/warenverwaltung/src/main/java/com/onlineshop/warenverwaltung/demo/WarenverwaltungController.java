a80cec622d0ba5f73d7aa5ce248c12ca1c4c1f9d
mitschl <Franke-michelle@gmx.de>
2019-06-06 19:41:34 +0200

    Warenverwaltungs Controller akualisiert

--- verteilte systeme/warenverwaltung/src/main/java/com/onlineshop/warenverwaltung/demo/WarenverwaltungController.java
+++ verteilte systeme/warenverwaltung/src/main/java/com/onlineshop/warenverwaltung/demo/WarenverwaltungController.java
@@ -34,5 +34,44 @@ public class WarenverwaltungController {
         return "Produkt succesfully created with id = " + id;
 
     }
+    @ResponseBody
+    @RequestMapping("/delete")
+    public String delete(int id){
+        try {
+            produktRepo.deleteById(id);
+        } catch (Exception e) {
+            return "Error beim Löschen des Produktes:" + e.toString();
+        }
+        return "Produkt wurde erfolgreich gelöscht!";
+    }
+    @ResponseBody
+    @RequestMapping("get-by-id")
+    public String getByEmail(int id) {
+        String userId = "";
+        try {
+            Produkt produkt = produktRepo.findByProduktId(id);
+            userId = String.valueOf(produkt.getArtikelnummer());
+        }
+        catch (Exception ex) {
+            return "Produkt wurde nicht gefunden";
+        }
+        return "Die Produkt Artikelnummer ist: " + userId;
+    }
+
+    @ResponseBody
+    @RequestMapping("/update")
+    public String updateUser(String name, int id, float Preis){
+        try{
+            Produkt produkt = produktRepo.findByProduktId(id);
+            produkt.setPreis(Preis);
+            produkt.setName(name);
+            produkt.setArtikelnummer(id);
+            produktRepo.save(produkt);
+
+        }catch(Exception e){
+            return "Error beim Aktualisieren"+e.toString();
+        }
+        return "Produkt wurde erfolgreich aktualisiert";
+    }
 
 }
