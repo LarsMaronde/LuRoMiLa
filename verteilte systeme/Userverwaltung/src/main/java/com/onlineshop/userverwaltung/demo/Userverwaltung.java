8c330f7093f3227f590e8a045abf14440340e47b
Ramasekva <lukas_konrad.schatz-linkenheil@student.reutlingen-university.de>
2019-06-06 18:38:18 +0200

    sstuff

--- verteilte systeme/Userverwaltung/src/main/java/com/onlineshop/userverwaltung/demo/Userverwaltung.java
+++ verteilte systeme/Userverwaltung/src/main/java/com/onlineshop/userverwaltung/demo/Userverwaltung.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.onlineshop.userverwaltung.demo.account.repository"})
 @EnableEurekaClient
 public class Userverwaltung {
 
