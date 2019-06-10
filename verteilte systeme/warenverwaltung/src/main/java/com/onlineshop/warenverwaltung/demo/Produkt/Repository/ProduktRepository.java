bc046aee1497c69029ef59f6607e9c113676c900
LarsMaronde <Lars.Maronde@Student.reutlingen-university.de>
2019-06-10 19:06:52 +0200

    reset branch

--- verteilte systeme/warenverwaltung/src/main/java/com/onlineshop/warenverwaltung/demo/Produkt/Repository/ProduktRepository.java
+++
@@ -1,9 +0,0 @@
-package com.onlineshop.warenverwaltung.demo.Produkt.Repository;
-
-import com.onlineshop.warenverwaltung.demo.Produkt.Model.Produkt;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProduktRepository extends CrudRepository<Produkt, Integer> {
-
-    Produkt findByProduktId(int id);
-}
